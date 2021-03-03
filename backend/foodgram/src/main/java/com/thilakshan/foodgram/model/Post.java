74a865e378d5f0282e0e09ef0a21b15562464b83
Thilakshan Baskaran <telakshanb@gmail.com>
2021-03-03 14:07:59 -0500

    created post model

--- backend/foodgram/src/main/java/com/thilakshan/foodgram/model/Post.java
+++ backend/foodgram/src/main/java/com/thilakshan/foodgram/model/Post.java
@@ -1,4 +1,32 @@
 package com.thilakshan.foodgram.model;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private String postName;
+
+    private String description;
+
+    private String postUrl;
+
+    private Integer voteCount = 0;
+
+
 }
